b600ab888bd5191499c6912620867638ab9f5922
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-04-25 22:42:20 +0200

    add utilities for js

---
+++ js/utilities.js
@@ -0,0 +1,38 @@
+//Fonction pour ajouter des elements dans le DOM
+function addElements(table){
+    const gallery = document.querySelector(".gallery")
+    for(element of table){
+        const figure = document.createElement("figure")
+        const image = document.createElement("img")
+        const title = document.createElement("figcaption")
+
+        image.src = element.imageUrl
+        image.alt = element.title
+        title.textContent = element.title
+
+        figure.appendChild(image)
+        figure.appendChild(title)
+        
+        gallery.appendChild(figure)
+    }    
+}
+
+//Fonction pour ajouter addEventListener sur les filtres
+function addClickEvent(element,data){
+    if(parseInt(element.dataset.id) === 0){
+        element.addEventListener("click",()=>{
+            document.querySelector(".gallery").innerHTML = ""
+            addElements(data)
+            document.querySelector(".selected").classList.remove("selected")
+            element.classList.add("selected")
+        })
+    }else{
+        element.addEventListener("click",()=>{
+            document.querySelector(".gallery").innerHTML = ""
+            const tableFilter = data.filter(res => res.categoryId === parseInt(element.dataset.id))
+            addElements(tableFilter)
+            document.querySelector(".selected").classList.remove("selected")
+            element.classList.add("selected")
+        })
+    }
+}
--- views/home/script.js
+++ views/home/script.js
@@ -1,50 +1,4 @@
-// console.log("Start")
 
-// const data = fetch("http://localhost:5678/api/works")
-//             .then(res => res.json())
-//             .then(res =>console.log(res))
-//             .catch(e => console.log(e))
-
-// console.log("End")
-
-//Fonction pour ajouter des elements dans le DOM
-function addElements(table){
-    for(element of table){
-        const gallery = document.querySelector(".gallery")
-        const figure = document.createElement("figure")
-        const image = document.createElement("img")
-        const title = document.createElement("figcaption")
-
-        image.src = element.imageUrl
-        image.alt = element.title
-        title.textContent = element.title
-
-        figure.appendChild(image)
-        figure.appendChild(title)
-        
-        gallery.appendChild(figure)
-    }    
-}
-
-//Fonction pour ajouter addEventListener sur les filtres
-function addClickEvent(element,data){
-    if(parseInt(element.dataset.id) === 0){
-        element.addEventListener("click",()=>{
-            document.querySelector(".gallery").innerHTML = ""
-            addElements(data)
-            document.querySelector(".selected").classList.remove("selected")
-            element.classList.add("selected")
-        })
-    }else{
-        element.addEventListener("click",()=>{
-            document.querySelector(".gallery").innerHTML = ""
-            const tableFilter = data.filter(res => res.categoryId === parseInt(element.dataset.id))
-            addElements(tableFilter)
-            document.querySelector(".selected").classList.remove("selected")
-            element.classList.add("selected")
-        })
-    }
-}
 async function executeCode(){
     try{
         //Recuperation des donnees
@@ -67,11 +21,8 @@ async function executeCode(){
         }
 
         //Ajout d'un click event sur les filtres
-        const allFilters = document.querySelectorAll(".categories li")
+        document.querySelectorAll(".categories li").forEach((category) => addClickEvent(category,result))
         
-        for(let i=0; i<allFilters.length;i++){
-            addClickEvent(allFilters[i],result)
-        }
     }catch(err){
         console.log(err)
     }
