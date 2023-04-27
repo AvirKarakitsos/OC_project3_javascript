383a630444a1d6065ca52904e9b248302430f25c
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-04-27 12:58:57 +0200

    add margin to header in connection.css

--- views/connection/connection.js
+++ views/connection/connection.js
@@ -12,6 +12,7 @@ form.addEventListener("submit",function(event){
     if((emailResponse.value !== "sophie.bluel@test.tld") || (passwordResponse.value !== "S0phie")){
         document.querySelector(".msg-error").innerHTML = "Erreur dans l'identifiant ou le mot de passe"
     }else{
+        document.querySelector(".msg-error").innerHTML = ""
         window.location.pathname = "/views/edit/edit.html"
     }
 })
